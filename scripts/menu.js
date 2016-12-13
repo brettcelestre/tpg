3a438b5b988ccdca5278a03040a4b149a0aceb96
Brett Celestre <bcelestre@gmail.com>
2016-12-12 18:29:50 -0800

    Completes mobile menu functionality

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,19 +1,29 @@
+
+
+var mobileMenu = function(){
+  $('.menu-icon').toggleClass('menu-icon-close');
+  $('.mobile-options').toggleClass('open');
+  
+  if ( $('.navigation').hasClass('fixed') ) {
+    $('.navigation').removeClass('fixed');
+    $('.home').css('margin-top', '0px');
+    $('.menu-fade').css('display', 'none');
+  } else {
+    $('.navigation').addClass('fixed');
+    $('.home').css('margin-top', '90px');
+    $('.menu-fade').css('display', 'block');
+  }
+}
+  
+  
 $(document).ready(function(){
   
   $('.menu-mobile').click(function(){
-    
-    $('.menu-icon').toggleClass('menu-icon-close');
-    $('.mobile-options').toggleClass('open');
-    
-    if ( $('.navigation').hasClass('fixed') ) {
-      $('.navigation').removeClass('fixed');
-      $('.home').css('margin-top', '0px');
-      $('.menu-fade').css('display', 'none');
-    } else {
-      $('.navigation').addClass('fixed');
-      $('.home').css('margin-top', '90px');
-      $('.menu-fade').css('display', 'block');
-    }
+    mobileMenu();
   });
   
+  $('.menu-fade').click(function(){
+    mobileMenu();
+  });
+
 });
