f7339892e2344dc01815061136e5899adce03bce
Brett Celestre <bcelestre@gmail.com>
2016-12-14 17:00:59 -0800

    Adds modals for bios

---
+++ scripts/bios.js
@@ -0,0 +1,181 @@
+var bios = {
+ 'Phil Costello': {
+    'title': 'Partner',
+    'bio': 'Central Valley grown, Phil’s grocery background is very diverse. The early years found him in Fresno area grocery stores. A total of eight years between Safeway and Save Mart working his way up through the ranks before departing as an Assistant Grocery Manager. Next came ten years with General Mills. Moved a total of seven times holding positions that ranged from Product Sales Manager for the Yoplait Division to Senior Key Account Manager/Safeway Team. From GMI, Phil played five years in the large brokerage arena managing both large and small clients (most notably the Minute Maid Co) for Kelley Clarke, Bradshaw, CROSSMARK, and Acosta. From there it was a couple years as Regional Sales Manager for Monterey Gourmet Foods managing small/mid size brokerages in the West. With nearly 30 years of grocery experience, all but two have been spent in the Northern California Market.' 
+  },
+  'Steve Martinez': {
+    'title': 'Partner',
+    'bio': 'Before joining Phil Costello and Tony Metz to form the Performance Group in Northern California in September of 2002, Steve spent just over 21 years working in the sales & marketing arena for two companies. The first 18 years were with Crossmark, who had purchased the Bradshaw North Company. Steve had the opportunity in the winter of 1995 to relocate to Salt Lake City to open up one of the first dedicated teams for American Stores as the General Manager of the Bradshaw American office. In 1998, he returned to Northern California as Vice President of Crossmark. The last three years prior to becoming a partner, Steve held the position of Director of the Safeway Team for Grocery & Confection for the Advantage Team. Steve has managed high profile grocery brands, and the last several years have been able to attract confection brands to TPG North due to an extensive background managing brands and departments. He is also very involved in several key Northern California organizations. Recently served as the President of the Northern California Sales & Marketing Organization, and has been an active member on the City of Hope Board of Directors for several years.'
+  },
+  'Jerry Jenson': {
+    'title': 'General Manager',
+    'bio': '<strong>1997 - 2011</strong><br>' +
+      '<div class="tab">Advantage Sales and Marketing<br>' +
+      'General Manager Northern California Division<br>' +
+      'GM/HBC Department Manager ASM Safeway Team<br>' + 
+      'Director of Retail Operations</div><br>' +
+      '<strong>1987 - 1997</strong><br>' +
+      '<div class="tab">Kelley Clarke Inc.<br>' +
+      'Vice President Retail Operations<br>' +
+      'Grocery Department Manager<br>' +
+      'Business Development Manager</div><br>' +
+      '<strong>1973 - 1987</strong><br>' +
+      '<div class="tab">Alpha Beta Supermarkets<br>' +
+      'GM/HBC Director for Northern California<br>' +
+      'Grocery Buyer/Merchandiser Northern California Division<br>' +
+      'Store Manager<br>' +
+      'Retail Clerk</div><br>' +
+      '<strong>Education:</strong><br>' +
+      '<div class="tab">California State University Bakersfield<br>' +
+      'Master of Science Business Administration<br>' +
+      'Bachelors Degree in Marketing</div><br>'
+  },
+  'Tony Metz': {
+    'title': 'Consultant',
+    'bio': 'Tony’s career in the food industry began in 1970 with The Drackett Products Company in Pittsburgh Pa. working through various positions ranging from Retail Sales Rep, National Sales Training to Western Division Manager. Tony joined Vlasic Foods in 1978 as Western Regional Manager and in 1981 began his career as a food broker joining Mancini Groesbeck in Northern California as an Account Executive and ultimately becoming President in 1986. In 1986 the Mancini Groesbeck Company was sold to Bradshaw Inc. where Tony spent the next six years serving as VP Sales and Executive VP & C.O.O. until the company was sold to CROSSMARK, Inc. in 2000. In 2002, Tony joined Phil Costello and Steve Martinez to form the Performance Group in Northern California. He annouced his retirement in 2011, but remains on as consultant.<br><br>' + 
+      'During Tony’s career, he has played an active role in the former NFBA as a committee member, as Past President of the Northern California Food Broker Assn. and also served on the Northern California’s City of Hope Board of Directors.'
+  },
+  'Steve Celestre': {
+    'title': 'Senior Business Manager',
+    'bio': "Steve Celestre started his career with the Alpha Beta company in 1971 as a courtesy clerk.  Steve moved his way up through the stores to Assistant Manager and was promoted into the Alpha Beta main office as their Corporate Scan Coordinator.<br><br>" + 
+      "After the merger with the Lucky store's, Steve was then promoted to the Lucky Health Care Category Manager in their San Leandro office where he worked until 1990 when he joined the Bradshaw Company.<br><br>" + 
+      "Working in the General merchandise department, Steve remained in the Brokerage business until 2002 when he took the position of Long's Candy Category Manager.<br><br>" + 
+      "Steve recently left Long's and has joined the Performance Group working with our Candy clients to help develop and expand their business."
+  },
+  'Julie Adam': {
+    'title': 'Senior Business Manager',
+    'bio': '<strong>1985 - 1996</strong><br>' +
+        '<div class="tab">Bradshaw:  Account Assistant, Customer Service Supervisor, Administrative Team Leader, Product Coordinator</div>' +
+        '<strong>1996 - 2001</strong><br>' +
+        '<div class="tab">Crossmark: Product Coordinator, Account Executive</div>' +
+        '<strong>2001 - 2006</strong><br>' +
+        '<div class="tab">Advantage Sales & Marketing:  Business Development Manager Northern California Markets, Business Manager Safeway Corporate</div>' +
+        '<strong>May 2006</strong><br>' +
+        '<div class="tab">The Performance Group:  Senior Business Manager</div>'
+  },
+  'Ken Moore': {
+    'title': 'Senior Business Manager',
+    'bio': 'In 2013 Ken joined The Performance Group of Northern California family and is looking forward to developing the brands he is responsible for and to furthering the relationships that have been established in the NorCal market place.<br><br>' +
+      'Ken Moore started his career in store management at Thrifty Drugs in 1989.  In 1994 Ken made the move into the broker world and has never left since.  Ken’s first experiences were in retail operations from schematic development, project management, retail sales while holding positions as Schematic Manager, Albertson’s Team Leader and Nestle Retail Business Manager while at Bromar/Marketing Specialists.  In 2001 Ken moved to Acosta as a Business Development Manager working on developing Geisha, Splenda, Glad, Newman’s Own and Morton Salt in the NorCal Market Place.  In 2010 Ken joined the Advantage Sales and Marketing team to develop Crystal Geyser Alpine Spring Water, Ghirardelli baking products, Hansen’s and Arm & Hammer Household products.  Ken’s current focus will be on grocery brands.'
+  },
+  'Dave Gonzales': {
+    'title': 'Senior Business Manager',
+    'bio': 'After graduating from San Jose State University, Dave started his career in sales and marketing with Cook – Harms & Le Baron (Now Advantage Sales & Marketing) in 1991 as a Territory Manager. Over his 22 years with Advantage, Dave held a number of positions to include Schematic Specialist, Key Account Manager, District Manager, Sales Manager Frozen Perishable and Business Development Manager for both the Perishable and Grocery Safeway teams.<br><br>' +
+      'Most recently Dave was a Sr. Business Development Manager with Impact Sales. Dave has managed a variety of manufactures to include ACH, Bellisio (Michelina’s), Challenge Dairy, Continental Mills, Don Francisco Coffee, Martinelli’s, Mrs. Smith’s, Tree Top, Rich Sea Pak, and Stahlbush Farms.'
+  },
+  'Ron Ingram': {
+    'title': 'Business Manager',
+    'bio': 'Before joining the Performance Group in June of 2003 Ron spent 19 years at MTC Distributing. Where he started out in the merchandising department adding new items and resetting stores. Ron joined the purchasing department a year later where he spent most of the time. He quickly advanced from product manager to senior product manager to director of purchasing and was responsible for partnering with manufacturer and the broker community to insure the growth of MTC. During his time as director of purchasing he developed several private label candy and snack items, implemented monthly sales flyers, contest for a sales staff of 30, new item introductions and manufacture relations. Ron serviced on the Nabisco C-Store advisory board from 1999 to 2001. His background in candy, snacks, beverages and other food items gives him solid experience for calling on C-Store distributors.'
+  },
+  'Jeremiah Hilton': {
+    'title': 'Customer Sales Manager',
+    'bio': 'In 2001 Jeremiah started his grocery retail career at Prism Retail Services where he spent the next 7 years as a Lead Team Leader. After leaving Prism, he joined Acosta as a Retail Merchandiser. before joining the Performance Group in 2012.'
+  },
+  'Rich Mott': {
+    'title': 'Business Manager',
+    'bio': 'Not available.'
+  },
+  'Dave Anderson': {
+    'title': 'Business Manager',
+    'bio': 'Before joining TPG in July 2009, Thad started his career with Safeway, Inc in 2002 in the Supply Chain department as a Procurement Specialist purchasing Non-Perishable products.  To continue to grow his career, he moved over to Category Management Department.  He started as a Category Analyst supporting the Laundry category and then the Paper Category.  He was promoted to Category Manager while working on the Paper team.  Thad was then promoted to Category Director on the Seasonal Desk, which included Charcoal/Logs, Apparel, Housewares, Home Decor, Automotive and Hardware.'
+  },
+  'Dawn Struer': {
+    'title': 'Customer Sales Manager',
+    'bio': 'I started my career with Carey Ahrens in 1978. After 7 years I joined J.J. Morris as a district manager in Sacamento and was promoted to Brand Manager and moved to Foster City. I was with them for 14 years which they then became Advantage Sales and Marketing. I left the brokerage business for 6 years and went to Safeway in there Corporate Brands division as a field assistant.  I went back to Advantage as a retail supervisor and shortly after joined Interlink Marketing in there Natural Division calling on Whole Foods and New Leaf until I joined TPG.'
+  },
+  'Jeff Morrison': {
+    'title': 'Customer Sales Manager',
+    'bio': 'Jeff has over 28 years of experience in the Food Industry. Jeff started working with an independent grocery chain called Foodland Markets in the Central Valley. His duties with Foodland ranged from starting as a Checker to running Dairy and HBC Departments in the stores, moving up to management.<br><br>' +
+      'In 1987, Jeff made a career move and began working for Carey-Ahrens, a Food Broker as a Territory Manager. Carey-Ahrens was purchased by Kelley-Clarke. Kelley-Clarke was purchased by Acosta Sales & Marketing. While working for Acosta for over 19 years, Jeff’s responsibilities grew to calling on the independent markets and Headquarter Accounts. Within the Independent Accounts Jeff was involved with the Grocery, Frozen, and HBC sections of the stores. Jeff has been involved in everything from long range planning for Headquarters to writing schematics for his accounts.<br><br>' +
+      'Jeff was offered a position of Business Manager for Performance Group in Northern California. His responsibilities include calling on Food 4 less & Food Maxx and major independents.<br><br>' +
+      'Jeff lives in Fresno California, with his wife Debbie.'
+  },
+  'Katie Day': {
+    'title': 'Business Manager',
+    'bio': "I started my sales career 1987 with Ernest & Julio Gallo as a Sales Rep. After three years I joined Bradshaw Inc (which became CROSSMARK) in 1990 where I worked in various sales & management positions over the next 10 years. Since 2004 I've been a full-time Realtor selling residential Real Estate for Coldwell Banker & Intero RE Services.<br><br>" + 
+      "I'm grateful to be back in the Food Brokerage business and look forward to the many challenges it brings."
+  },
+  'Rich Amorosa': {
+    'title': 'Customer Sales Manager',
+    'bio': "Rich has over 30 years experience in the northern California food industry.  Rich moved to Northern California in 1976, coming from the Big Apple.  He started his career with Johnston's Yogurt Co. in sales and delivery.  In 1977, he was promoted to the position of area manager.  In 1979, Johnston's decided to go broker and joined the Bradshaw North team, and so did Rich, as a territory manager.  In 1980, Rich was promoted to Key Account Manager.  Over the next several years his headquarter responsibilities included Fry's Food Stores, Nob Hill Foods Stores, Savemart HQ, Certified Grocers (Unified Grocers), SSI and numerous smaller HQ groups.  Rich was then promoted to Business Manager in 1997, and was responsible for lines, such as, Lea&Perrins, Sue Bee Honey, Eagle Brand Milk, just to name a few.  In 2001, Rich left Bradshaw, which had then become CROSSMARK, and went to work for Boyd’s Coffee Co. in Northern California, which eventually led to his move to the Reno/Sparks with Boyd’s.<br><br>" +
+      "Rich joined the Performance Group in October 2008 and will be responsible for our business in the Reno, Nevada area."
+  },
+  'Sherry Copeland': {
+    'title': 'Customer Sales Manager',
+    'bio': 'Sherry has 25 years of experience in the grocery industry. She started her career in 1989 as a route drive for the Keebler Co. In 1993 she move to the Midwest and joined PMI-Eisenhard food brokers as a sales represntative and doing headquarter calls for independent accounts in Missouri and Illinois.<br><br>' +
+      'Sherry moved back to Northern California in 1997 and spent the next 17 years as sales merchandiser for local food brokers, until she joined the Performance Group in 2014.'
+  },
+  'Adam Martinez': {
+    'title': 'Customer Sales Manager',
+    'bio': 'Not available.'
+  },
+  'Charmaine Murphy': {
+    'title': 'Customer Sales Manager',
+    'bio': 'Charmaine left Montgomery Wards to begin her grocery career with Bradshaw in 1990 as a retail merchandiser. During her 11 years with Bradshaw/CROSSMARK, she met and married her husband . Charmaine spent the next 12 years as part of the Acosta retail sales team, before joining the Performance Group in 2013.'
+  },
+  'Laurie Withers': {
+    'title': 'Office Manager',
+    'bio': 'In 1995, Laurie started her career in the Food Industry at Bradshaw Inc. as an Administrative Assistant.<br><br>' +
+      'Laurie was promoted to Administrative Grocery Team Leader in 1998, managing a staff of 10 Administrative Personnel.<br><br>' +
+      'Laurie left Bradshaw/CROSSMARK in 2000 and joined Advantage Sales & Marketing as an Administrative Manager, managing a staff of 8 Administrative Personnel.<br><br>' +
+      'Laurie left Advantage in 2002 to join the Performance Group as their Office Manager.'
+  },
+  'Diane Colli': {
+    'title': 'Administrative Supervisor',
+    'bio': 'I started working in the grocery industry going to work for Tracy Food 4 Less as a cashier. After approx. 2 years of checking I was promoted to an Administrative Assistant handling various office and clerical duties which evolved into working and assisting the buyer. In 2005 these 4 Food 4 Less stores were purchased by Food Maxx and the buying office would be handled by Food Maxx’s Corporate office and the position was eliminated. In March of 2005 I went to work for Acosta as an Administrative Assistant on the Safeway Team and in July of 2006 came to work for the Performance Group.'
+  },
+  'Stacy Humes': {
+    'title': 'Account Assistant',
+    'bio': 'Stacy started her career in the mailroom at Bradshaw Inc.-North in 1989. Over the next 14 years she advanced to various Customer Service and Administrative Assistant positions, and in 2002 she became Safeway Administrative Team Leader. In 2003, she left CROSSMARK to join the Performance Group as Administrative / Customer Service Assistant.'
+  },
+  'Marta Valdez': {
+    'title': 'Account Assistant',
+    'bio': 'Marta began working within the Warehousing and Transportation industry in 1980 as Freight Clerk at J&R Warehouses, Hayward CA.  From there moving  into positions in Customer Service and Accounting.  She left J&R in 1985 and started a small family owned transportation company, MKJ & Daughters, Inc.  In 2003 she worked in truck brokerage as dispatcher/ customer service representative, working for Pacer Global Logistics until 2005, and HA Logistics until 2007.  In 2008 she proudly joined the team at the Performance Group as Administrative/ Customer Service Assistant.'
+  },
+  'Deanna Church': {
+    'title': 'Administrative Assistant',
+    'bio': '<strong>23.5 Years with Longs Drugs Stores:</strong><br>' +
+      'Began as an Order Desk Processor of orders from 100+ stores to the warehouse in 1985. Moved to Southern California to work in a District Office for 13 years 1988 as District Office Admin Assistant. Worked with 30 stores for district Pricing, bought and distributed products for a bi-monthly ad with purchase orders for various distributors. Moved back to the Bay Area in 2001, to work at the Corp Office in Walnut Creek, as Ad Scan Admin for the Advertising Dept. Worked with the Category Managers on the weekly ads, Ad Pricing for all the 6 Pricing Zones for 500+ stores.  The job ended in May 2009 when CVS bought Longs.<br><br>' +
+      '<strong>14 Months with Acosta Sales & Marketing:</strong><br>' +
+      'Worked as a Claims Coordinator.  Validating over $275k monthly in deduction claims for 5 major clients.'
+  },
+  'Sara Lund': {
+    'title': 'Administrative Assistant',
+    'bio': 'Sara began her career in 1983 working for JJ Morris.  Throughout the 15 years of working in the food industry, she also worked for Kelley-Clarke and Marketing Specialists (formerly Bromar).  Her responsibilities included administrative assistant to several business managers, retail sales assistant, supervisor of the mailroom, and supervisor of the “Vista” (promotions) department.<br><br>' +
+    'Sara “took a break” from the food industry and worked as an executive assistant at ADP for 10 years.  She also learned how to hand craft a “mean cup of coffee” as a part-time barista / shift supervisor at Starbucks for 4 years.<br><br>' +
+    'In 2012 she made the decision to return to the food industry and came to work for TPG as Administrative / Customer Service Assistant.'
+  }
+}
+
+$(document).ready(function(){
+  
+  var $exit = $('.exit-svg'),
+      $modal = $('.bio-modal'),
+      name;
+  
+  // Opens modal
+  $('.member').click(function(e){
+    // img = this.children[0].attributes[0].value,
+    name = this.children[1].innerText,
+    title = bios[name].title,
+    bio = bios[name].bio;
+  
+    // Update modal data
+    $('.bio').html('<div class="bio-name">' + name + '</div><span class="job-title">' + title + '</span><br><div class="bio-copy">' + bio + '</div>');
+    // Shows modal
+    $('.bio-modal').css('display', 'block');
+  });
+  
+  // Closes modal with exit button
+  $exit.click(function(){
+    $('.bio-modal').css('display', 'none');
+  });
+  // Closes modal by click outside of modal
+  window.onclick = function(event) {
+      if (event.target.className == 'bio-modal') {
+          $('.bio-modal').css('display', 'none');
+      }
+  }
+  
+});
+
